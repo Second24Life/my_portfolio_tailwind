18d142f0a14753e55835d9adcff3f9db660dacad
Max Zarubitskiy <maka123456789@mail.ru>
2023-08-30 02:24:44 +0700

    first version completed

---
+++ pages/_app.jsx
@@ -0,0 +1,25 @@
+import "../styles/globals.css";
+
+// Components
+import Layout from "../components/Layout";
+import Transition from "../components/Transition";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className='h-full'>
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
+  );
+}
+
+export default MyApp;

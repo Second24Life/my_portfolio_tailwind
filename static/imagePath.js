84a84547d9dbe341963b30c62bdb4f92b424eefa
Max Zarubitskiy <maka123456789@mail.ru>
2024-02-22 18:07:16 +0700

    added project images

---
+++ static/imagePath.js
@@ -0,0 +1,74 @@
+// export const KeyToAiImage = [
+//   "keytoai1.png",
+//   "keytoai2.png",
+//   "keytoai3.png",
+//   "keytoai4.png",
+//   "keytoai5.png",
+//   "keytoai6.png",
+//   "keytoai7.png",
+//   "keytoai8.png",
+//   "keytoai9.png",
+//   // "keytoai10.png",
+//   // "keytoai11.png",
+//   // "keytoai12.png",
+// ]
+
+export const edleedImage = [
+  "/edleed/edleed1.png",
+  "/edleed/edleed2.png",
+  "/edleed/edleed3.png",
+  "/edleed/edleed4.png",
+  "/edleed/edleed5.png",
+  "/edleed/edleed6.png",
+  "/edleed/edleed7.png",
+  "/edleed/edleed8.png",
+  "/edleed/edleed9.png",
+  // "edlee10.png",
+  // "edlee11.png",
+  // "edlee12.png",
+]
+
+// export const ShortLinksImage = [
+//   "shortlinks1.png",
+//   "shortlinks2.png",
+//   "shortlinks3.png",
+//   "shortlinks4.png",
+//   "shortlinks5.png",
+//   "shortlinks6.png",
+//   "shortlinks7.png",
+//   "shortlinks8.png",
+//   "shortlinks9.png",
+//   // "shortlinks10.png",
+//   // "shortlinks11.png",
+//   // "shortlinks12.png",
+// ]
+
+export const oxweImage = [
+  '/oxwe/oxwe1.png',
+  '/oxwe/oxwe2.png',
+  '/oxwe/oxwe3.png',
+  '/oxwe/oxwe4.png',
+  '/oxwe/oxwe5.png',
+  '/oxwe/oxwe6.png',
+  '/oxwe/oxwe7.png',
+  '/oxwe/oxwe8.png',
+  '/oxwe/oxwe9.png',
+  // "oxwe10.png",
+  // "oxwe11.png",
+  // "oxwe12.png",
+]
+
+export const argusImage = [
+  "/argus/argus1.png",
+  "/argus/argus2.png",
+  "/argus/argus3.png",
+  "/argus/argus4.png",
+  "/argus/argus5.png",
+  "/argus/argus6.png",
+  "/argus/argus7.png",
+  "/argus/argus8.png",
+  "/argus/argus9.png",
+  // "argus10.png",
+  // "argus11.png",
+  // "argus12.png",
+]
--- static/index.js
+++ static/index.js
@@ -10,6 +10,9 @@ import {
   SiTrello,
   SiTypescript,
 } from "react-icons/si";
+import { argusImage, edleedImage, oxweImage } from "./imagePath";
+
+export * from "./imagePath";
 
 const styleIcons =
   "group-hover:ease-in-out transition duration-700 group-hover:duration-1000 cursor-pointer object-center object-cover group-hover:scale-110 sm:group-hover:scale-150  aspect-square rotate-0 group-hover:-rotate-[360deg] hover:fill-accent group-hover:-translate-y-6 group-hover:-skew-y-6 group-hover:skew-x-6";
@@ -84,77 +87,29 @@ export const experience = [
   },
 ];
 
-export const KeyToAiImage = [
-  "keytoai1.png",
-  "keytoai2.png",
-  "keytoai3.png",
-  "keytoai4.png",
-  "keytoai5.png",
-  "keytoai6.png",
-  "keytoai7.png",
-  "keytoai8.png",
-  "keytoai9.png",
-  // "keytoai10.png",
-  // "keytoai11.png",
-  // "keytoai12.png",
-]
-
-export const edleedImage = [
-  "edlee1.png",
-  "edlee2.png",
-  "edlee3.png",
-  "edlee4.png",
-  "edlee5.png",
-  "edlee6.png",
-  "edlee7.png",
-  "edlee8.png",
-  "edlee9.png",
-  // "edlee10.png",
-  // "edlee11.png",
-  // "edlee12.png",
-]
-
-// export const ShortLinksImage = [
-//   "shortlinks1.png",
-//   "shortlinks2.png",
-//   "shortlinks3.png",
-//   "shortlinks4.png",
-//   "shortlinks5.png",
-//   "shortlinks6.png",
-//   "shortlinks7.png",
-//   "shortlinks8.png",
-//   "shortlinks9.png",
-//   // "shortlinks10.png",
-//   // "shortlinks11.png",
-//   // "shortlinks12.png",
-// ]
-
-export const oxweImage = [
-  "oxwe1.png",
-  "oxwe2.png",
-  "oxwe3.png",
-  "oxwe4.png",
-  "oxwe5.png",
-  "oxwe6.png",
-  "oxwe7.png",
-  "oxwe8.png",
-  "oxwe9.png",
-  // "oxwe10.png",
-  // "oxwe11.png",
-  // "oxwe12.png",
-]
-
-export const argusImage = [
-  "argus1.png",
-  "argus2.png",
-  "argus3.png",
-  "argus4.png",
-  "argus5.png",
-  "argus6.png",
-  "argus7.png",
-  "argus8.png",
-  "argus9.png",
-  // "argus10.png",
-  // "argus11.png",
-  // "argus12.png",
-]
+export const projectImages = [
+  {
+    title: "Edlee",
+    images: edleedImage,
+    link: "https://edleed.com/",
+  },
+  {
+    title: "Oxwe",
+    images: oxweImage,
+    link: "https://oxwe.ru/",
+  },
+  {
+    title: "Argus",
+    images: argusImage,
+    link: "https://argus-company.ru/",
+  },
+  {
+    title: "Различные проекты",
+    images: argusImage,
+    link: ''
+  }
+  // {
+  //   title: "ShortLinks",
+  //   images: ShortLinksImage,
+  // },
+];
